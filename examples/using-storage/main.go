19b67939763261d6fee32f2e19c2ad563239d31b
zhudz <zdzserver@163.com>
2023-11-18 19:47:53 +0800

    add raw

--- examples/using-storage/main.go
+++ examples/using-storage/main.go
@@ -23,13 +23,38 @@ func main() {
 	dir, _ := os.Getwd()
 	fmt.Printf("[%s] %s\n", dir, e.String())
 
-	if err = e.Run("echo hello world"); err != nil {
+	e.AddRawCommand([]byte(`
+
+# check user privileges
+root_privs() {
+  if [[ $(id -u) -eq 0 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# check for required files and deps first
+# check if command exists
+command_exists() {
+  type "${1}" > /dev/null 2>&1
+}
+
+if (command_exists ls); then
+   echo "command_exists: ls"
+fi
+
+`))
+
+	if err = e.Run(); err != nil {
 		fmt.Printf("exec fail:%s\n", err)
 	}
 }
 
 /*
 [go-sh] /bin/bash -ex -o pipefail /tmp/clc9uvco47mm9mrmcbfg
-+ echo hello world
-hello world
++ command_exists ls
++ type ls
++ echo 'command_exists: ls'
+command_exists: ls
 */
